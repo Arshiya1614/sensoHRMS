2a50f1ee4f138573f324330431a61945df125773
arshiya <arshiyazade@gmail.com>
2024-11-18 13:40:45 +0530

    forgot and rest-pwd page done

--- src/app/authentication/pages/forgot/forgot.component.ts
+++ src/app/authentication/pages/forgot/forgot.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router , NavigationExtras} from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
+// import { EncryptService } from 'src/app/services/encrypt.service';
 
 @Component({
   selector: 'app-forgot',
@@ -6,5 +11,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./forgot.component.css']
 })
 export class ForgotComponent {
+  constructor(
+    private snackBar: MatSnackBar,
+    private router : Router,
+    private authService: AuthService,
+    // private encryptService: EncryptService
+  ){}
 
+email = new FormControl('', [Validators.required, Validators.email]);
+errorMessage = '';
+loading:boolean = false;
+loadingMessage: string  = "Submit";
+getErrorMessage() {
+  if (this.email.hasError('required')) {
+    return 'Email is required';
+  }
+
+  return this.email.hasError('email') ? 'Not a valid email' : '';
+}
+submit(){
+  if (this.email.valid) {
+    this.loading = true;
+    this.loadingMessage = "Submitting...";
+    const forgotData = {
+      personalEmail: this.email.value,
+    };
+    // this.authService.forgotPassword(forgotData).subscribe(
+    //   () => {
+    //     const personalEmail = forgotData.personalEmail;
+    //     this.redirectToMailSend(personalEmail);
+    //     this.snackBar.open('Reset Password Link send successful!', 'Dismiss', {
+    //       duration: 2000
+    //     });
+    //   },
+    //   (error) => {
+    //     this.snackBar.open(
+    //       error.error.message || 'Failed to send Link. Please try again.',
+    //       'Dismiss',
+    //       { duration: 2000 }
+    //     );
+    //     this.errorMessage = error.error.message || '';
+    //     this.loading = false;
+    //     this.loadingMessage = "Submit";
+    //   }
+    // );
+  }
+}
+
+redirectToMailSend(personalEmail: string | null) {
+  // if (personalEmail) {
+  //    const encryptedEmail = this.encryptService.encryptData(personalEmail);
+  //    const queryParams = {
+  //     personalEmail: encryptedEmail 
+  //   };
+  //   const navigationExtras: NavigationExtras = {
+  //     queryParams: queryParams
+  //   };
+  //   this.router.navigate(['/authentication/mail-send'], navigationExtras);
+  // } else {
+  //   console.error('Personal email is null');
+  // }
+}
 }
--- src/app/authentication/pages/reset-pwd/reset-pwd.component.ts
+++ src/app/authentication/pages/reset-pwd/reset-pwd.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators ,ReactiveFormsModule, FormControl} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from '../../auth/auth.service';
+
 
 @Component({
   selector: 'app-reset-pwd',
   templateUrl: './reset-pwd.component.html',
   styleUrls: ['./reset-pwd.component.css']
 })
-export class ResetPwdComponent {
+export class ResetPwdComponent implements OnInit {
+  token!: string;
+  hidePassword = true;
+  hideConfirmPassword = true;
+  confirmPassword = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
+ 
+  constructor(
+    private authService : AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar
+  ) {}
 
+  ngOnInit(): void {
+    this.token = this.route.snapshot.queryParams['token'];  }
+
+  getPasswordErrorMessage() {
+    if (this.password.hasError('required')) {
+      return 'Password is required';
+    }
+    return this.password.hasError('minlength')
+      ? 'Password should be at least 8 characters long'
+      : '';
+  }
+
+  getConfirmPasswordErrorMessage() {
+    if (this.confirmPassword.hasError('required')) {
+      return 'Confirm Password is required';
+    }
+    if (this.confirmPassword.hasError('minlength')) {
+      return 'Password should be at least 8 characters long';
+    }
+    if (this.password.value !== this.confirmPassword.value) {
+      return 'Passwords do not match';
+    }
+    return '';
+  }
+ 
+
+  submit(){
+    if (this.token) {
+      const resetData = { 
+        token: this.token ,
+        password: this.password.value
+      };
+      // this.authService.resetPassword(resetData)
+      //   .subscribe(
+      //     () => {
+      //       this.snackBar.open('Password Update Successfully!', 'Dismiss', {
+      //           duration: 2000
+      //         });
+      //       this.redirectToLoginPage();
+      //     },
+      //     error => {
+      //       this.snackBar.open(
+      //         error.error.message || 'Failed to update password!',
+      //         'Dismiss',
+      //         { duration: 2000 }
+      //       );
+      //       this.redirectToLoginPage();
+      //     }
+      //   );
+    } else {
+      this.snackBar.open('Token not found', 'Dismiss', {
+        duration: 2000
+      });
+      this.redirectToLoginPage();
+    }
+  }
+    
+  redirectToLoginPage() {
+    setTimeout(() => {
+      this.router.navigate(['/authentication/login']);
+    }, 2000); 
+  }
 }
